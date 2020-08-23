9c9d56b91f8185d82eefbfb30fd167fc4d98f7f3
SPahooja <pahoojasahil@gmail.com>
2020-08-23 19:26:28 +0530

    Added skeleton

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module GOt
+module got
 
 go 1.15
+
+require github.com/muesli/termenv v0.7.2
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "got/got"
+
+
+//CProfile := termenv.ColorProfile()
+
+
+func main() {
+	got.NewGot()
+}
